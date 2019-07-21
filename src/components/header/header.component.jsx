5b2ec71a29a430bc42dc56508d93b8b4d2116f8c
imfarhad <fakhrul.farhad@gmail.com>
2019-07-21 21:54:17 +0300

    added cartIcon and cartDropDown components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,11 +5,14 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'; 
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => ( //passing currentUser as a prop, received via mapStatesToProps
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -23,16 +26,22 @@ const Header = ({currentUser}) => (
             </Link>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+                    (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
+                    :
+                    (<Link className='option' to='/signin'>SIGN IN</Link>)
+            }            
+            <CartIcon />
         </div>
+        {
+            hidden ? null : 
+            <CartDropDown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
     currentUser: null
 }
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type){
+    switch (action.type) {
         case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
